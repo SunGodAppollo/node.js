d38a201953c0c8a7022b4997f23c81fa78551c30
公司 <417045639@qq.com>
2018-09-30 14:15:47 +0800

    node简易留言板

--- 2-2node留言板/node留言板.js
+++ 2-2node留言板/node留言板.js
@@ -1,14 +1,41 @@
 var http=require('http');
+var url=require('url');//路径解析
 var fs=require('fs');
 var template=require('art-template');
 
 var viewPath='./view';
 
+//设置全局信息存储
+ var messageData=[
+         {name:'Tom',
+          message:'I think this is Good',
+          date: '2018-9-29'
+        },
+        {name:'Jerry',
+         message:'I su',
+         date: '2018-9-29'
+       },
+     ];
+
 var server=http.createServer(function(request,response){
    var requestUrl=request.url;
+   var urlObj=url.parse(requestUrl,true);
+   requestUrl=urlObj.pathname;
+
    var filePath='/index.html';
    if(requestUrl!='/'){
      filePath=requestUrl;
+   }
+   //查看是否是添加信息-特殊
+   if(requestUrl=='/addMeg'){
+	   var addData=urlObj.query;
+     addData.date='2018-9-30';
+     messageData.push(addData);
+     //重定向
+     response.statusCode=302;
+     response.setHeader('Location','/');
+     response.end();
+	   return ;
    }
 
    requestPath=viewPath+filePath;
@@ -30,16 +57,7 @@ var server=http.createServer(function(request,response){
      if(error){
        response.end('404 Not Find Page');
      }else{
-       var messageData=[
-         {name:'Tom',
-          message:'I think this is Good',
-          date: '2018-9-29'
-        },
-        {name:'Jerry',
-         message:'I su',
-         date: '2018-9-29'
-       },
-     ];
+
        var tplFile=template.render(data.toString(),{dataList:messageData});
         response.end(tplFile);
      }
