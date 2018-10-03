88f9d6507ae4f65149c88560a1f61c23b7cdfbe5
狐狸 <417045639@qq.com>
2018-10-03 16:48:49 +0800

    Express 公共资源配置

---
+++ 4-1Express/Express初识/README.md
@@ -0,0 +1,13 @@
+#  安装nodemon自动重启程序
+<hr>
+## 安装方式
+``
+npm i --global nodemon
+``
+
+## 启动node.js程序就可以直接
+``
+nodemon  file.js
+``
+
+## 这样启动后改变node.js文件后 nodemon会自动帮助我们重启
--- 4-1Express/Express开发静态资源访问/Express开发静态资源访问.js
+++ 4-1Express/Express开发静态资源访问/Express开发静态资源访问.js
@@ -7,7 +7,20 @@ const app=express();
 
 //设置可访问目录
 //开发公共资源
-app.use('/public/',express.static('./public'));
+//http://127.0.0.1:3000/public/img.png
+//app.use('/public/',express.static('./public'));
+
+
+//也可以设置成多层的目录
+//http://127.0.0.1:3000/a/b/c/img.png
+//app.use('/a/b/c/',express.static('./public'));
+
+
+//当省略第一个参数的时候 默认请求路径是 '/'
+//http://127.0.0.1:3000/img.png
+app.use(express.static('./public'));
+
+
 
 //根据请求配置
 app.get('/',function(req,res){
